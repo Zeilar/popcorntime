f855f394d031343466e8458872971796a3dc1791
Philip <philip_angelin@hotmail.com>
2021-11-09 14:27:52 +0100

    Styling

--- client/src/domains/admin/components/dashboard/DashboardItem.tsx
+++ client/src/domains/admin/components/dashboard/DashboardItem.tsx
@@ -25,7 +25,7 @@ export default function DashboardItem({ to, children, icon }: IProps) {
             transition="background-color 0.05s"
             _hover={{ bgColor: "gray.500" }}
             _active={{ bgColor: theme.colors.gray["300"] }}
-            _activeLink={{ bgColor: theme.colors.gray["100"] }}
+            _activeLink={{ bgColor: theme.colors.brand["default"] }}
             p="0.5rem"
             borderRadius="base"
         >
