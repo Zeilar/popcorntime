d3b887a209432d9a0821a17b9fa49f9764c357e4
Philip <philip_angelin@hotmail.com>
2021-11-20 19:27:52 +0100

    Stronger shadows

--- client/src/domains/common/theme/shadows.ts
+++ client/src/domains/common/theme/shadows.ts
@@ -6,11 +6,11 @@ export const shadows = {
     lg: "0 0 8px 0 rgba(0, 0, 0, 0.5)",
     xl: "0 0 12px 0 rgba(0, 0, 0, 0.75)",
     elevate: {
-        bottom: "0 1px 2px 0 rgba(0, 0, 0, 0.75)",
-        top: "0 -1px 2px 0 rgba(0, 0, 0, 0.75)",
-        left: "-1px 0 2px 0 rgba(0, 0, 0, 0.75)",
-        right: "1px 0 2px 0 rgba(0, 0, 0, 0.75)",
-        all: "0 0 3px 0 rgba(0, 0, 0, 0.75)",
+        bottom: "0 1px 2px 0 rgba(0, 0, 0, 0.85)",
+        top: "0 -1px 2px 0 rgba(0, 0, 0, 0.85)",
+        left: "-1px 0 2px 0 rgba(0, 0, 0, 0.85)",
+        right: "1px 0 2px 0 rgba(0, 0, 0, 0.85)",
+        all: "0 0 2px 0 rgba(0, 0, 0, 0.85)",
     },
     outline: `0 0 0 2px ${colors.brand.light}`,
 };
