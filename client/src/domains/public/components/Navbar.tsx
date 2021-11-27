b2058c79734794b75c05ff3f39c11a4a9112b66a
Philip <philip_angelin@hotmail.com>
2021-11-27 02:43:19 +0100

    Added socket name to navbar

--- client/src/domains/public/components/Navbar.tsx
+++ client/src/domains/public/components/Navbar.tsx
@@ -1,19 +1,34 @@
-import { Flex } from "@chakra-ui/layout";
+import { Flex, Text } from "@chakra-ui/layout";
 import BrandLogo from "domains/common/components/styles/BrandLogo";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { MeContext } from "../contexts";
 
 export default function Navbar() {
+    const { me } = useContext(MeContext);
     return (
         <Flex
             as="nav"
             boxShadow="elevate.bottom"
             bgColor="gray.800"
             zIndex={1000}
-            p="1rem"
+            alignItems="center"
+            py="1rem"
+            px="2rem"
         >
             <Link to="/">
                 <BrandLogo />
             </Link>
+            {me && (
+                <Text
+                    as="h2"
+                    ml="auto"
+                    fontWeight={600}
+                    color={`${me.color}.600`}
+                >
+                    {me.username}
+                </Text>
+            )}
         </Flex>
     );
 }
