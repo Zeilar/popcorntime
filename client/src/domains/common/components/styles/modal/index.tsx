9d461c4a9a677798306edffc0fdcaa6533164c8d
Philip <philip_angelin@hotmail.com>
2021-12-05 08:39:27 +0100

    Fixes

--- client/src/domains/common/components/styles/modal/index.tsx
+++ client/src/domains/common/components/styles/modal/index.tsx
@@ -2,9 +2,8 @@ import ModalBody from "./Body";
 import ModalHeader from "./Header";
 import ModalContent from "./Content";
 import ModalOverlay from "./Overlay";
-import { Box, Flex, FlexProps } from "@chakra-ui/layout";
+import { Flex, FlexProps } from "@chakra-ui/layout";
 import { useEffect } from "react";
-import { AnimatePresence } from "framer-motion";
 import { useOnClickOutside } from "domains/common/hooks";
 
 interface IModalProps {
@@ -35,29 +34,22 @@ export default function Modal(props: IModalProps) {
         };
     }, []);
 
-    return (
-        <Box ref={wrapperEl}>
-            <AnimatePresence>
-                {props.isOpen && (
-                    <Flex
-                        exit={{ top: "8rem", opacity: 0 }}
-                        animate={{ top: "10rem", opacity: 1 }}
-                        top="8rem"
-                        pos="fixed"
-                        transform="translateX(-50%)"
-                        left="50%"
-                        bgColor="primary.darkest"
-                        zIndex={100000}
-                        rounded="base"
-                        boxShadow="0 0 30px 0 rgba(0, 0, 0, 0.5)"
-                        {...props.style}
-                    >
-                        {props.children}
-                    </Flex>
-                )}
-            </AnimatePresence>
-        </Box>
-    );
+    return props.isOpen ? (
+        <Flex
+            ref={wrapperEl}
+            top="10rem"
+            pos="fixed"
+            transform="translateX(-50%)"
+            left="50%"
+            bgColor="primary.darkest"
+            zIndex={100000}
+            rounded="base"
+            boxShadow="0 0 30px 0 rgba(0, 0, 0, 0.5)"
+            {...props.style}
+        >
+            {props.children}
+        </Flex>
+    ) : null;
 }
 
 Modal.Body = ModalBody;
